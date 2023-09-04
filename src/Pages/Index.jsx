d296cdfe6d56a283325780de560f80e3b14334b4
Somu050600 <somasekhareega@gmail.com>
2023-09-04 17:24:27 +0530

    overflowX -hidden on mobile

--- src/Pages/Index.jsx
+++ src/Pages/Index.jsx
@@ -1,22 +1,21 @@
 import Contact from "./Contact/Contact";
 import LandingPage from "./LandingPage/LandingPage";
 import About from "./About/About";
-import './index.css';
-
+import "./index.css";
 
 function Pages() {
-    return (
-      <div>
-        <LandingPage/>
-        <About/>
-        <Contact/>
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-      </div>
-    );
-  }
-  
-  export default Pages;
+  return (
+    <div style={{ maxWidth: "100vw", overflowX: "hidden" }}>
+      <LandingPage />
+      <About />
+      <Contact />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+    </div>
+  );
+}
+
+export default Pages;
